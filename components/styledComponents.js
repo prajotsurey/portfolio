fd8a888c925f078b4f2770b2d658098ccf2d91f7
prajotsurey <prajotsurey@gmail.com>
2022-02-10 19:06:52 +0530

    added minheight to container

--- components/styledComponents.js
+++ components/styledComponents.js
@@ -7,11 +7,12 @@ export const SectionContainer = styled.section`
   flex-direction: column;
   margin-left: auto;
   margin-right: auto;
+  min-height: 700px;
 `
 
 export const SectionHeading = styled.h2`
   font-weight: 200;
   font-size: 2.5rem;
   margin-top: 2rem;
-  margin-bottom: 8rem; 
+  margin-bottom: 8rem;
 `
