4e6c0cde4ba1d6f4bee34dfad91be47629608d98
prajotsurey <prajotsurey@gmail.com>
2022-02-14 19:36:43 +0530

    placed totoro

--- components/HeroLandingSection.js
+++ components/HeroLandingSection.js
@@ -1,28 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
+import Totoro from './Totoro'
 
 const HeroContainer = styled.section`
   width: 90%;
   max-width: 1500px;
   height: calc(100vh - 70px);
   display: flex;
-  flex-direction: row;
+  flex-direction: column-reverse;
   align-items:center;
-  max-width: ;
+  justify-content:center;
   margin-left: auto;
   margin-right: auto;
+  
+  @media(min-width:600px) {
+    flex-direction: row;
+  }
 `
 
 const WelcomeText = styled.h1`
   width: 20ch;
   font-weight: 700;
   font-family: 'Roboto', sans-serif;
-  margin-top: -140px;
-
   font-size: calc(1.5rem + 1.5vw);
+  flex-shrink:0;
+  @media(min-width:600px) {
+    margin-top: -140px;
+  }
 
   @media(min-width:1200px) {
     font-size: 4rem;
+  }
+`
+
+const TotoroContainer = styled.div`
+  display:flex;
+  flex-direction:row;
+  justify-content:center;
+  width: 80%;
+  @media(min-width:600px) {
+    margin-top: -140px;
+    flex-grow:1;
   }
 `
 
@@ -34,6 +52,9 @@ const HeroLandingSection = ({blok}) => {
         {blok.greeting}<br/>
         {blok.introText}
       </WelcomeText>
+      <TotoroContainer>
+        <Totoro />
+      </TotoroContainer>
     </HeroContainer>
   )
 } 
